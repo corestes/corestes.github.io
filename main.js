87cd14663d828cfe6713aba7aa76e2dbd348a3f2
corestes <leyatate@gmail.com>
2022-12-04 23:56:56 +0600

    -

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function namefunction () {
+    let visitorname = document.getElementById("name").value;
+    document.getElementById("tobechanged").innerHTML = "Hello, " + visitorname + "<br> You can conact me at: <br> Institutional e-mail: leya.timur@nu.edu.kz <br> Personal e-mail: leyatate@gmail.com <br> Phone number: +7 775 228 62 11 <br> Telegram: @wizardhubris <br>";
+    document.getElementById("vklink1").style.color= "black";
+    document.getElementById("vklink2").style.color= "black";
+}
+
+function showskills () { document.getElementById("hidden").style.color= "#05372b";}
